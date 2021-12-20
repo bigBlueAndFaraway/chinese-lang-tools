1b2dc583b2f7c5c38a51bd51d979153f18b947c4
Lennart Scharmann <lennart.scharmann@sellerx.onmicrosoft.com>
2021-12-20 23:35:02 +0100

    added function filter_by_level

--- hsk_tools.py
+++ hsk_tools.py
@@ -31,12 +31,12 @@ class hskInterpreter():
         return max([self.char_lvl(c) for c in sentence])
 
 
-    def column_lvl(self, column):
+    def column_lvl(self, hanyu_col):
         '''applies sentence-level assignment of hsk-level to a whole column'''
-        return column.apply(lambda x: self.sentence_lvl(x))
+        return hanyu_col.apply(lambda x: self.sentence_lvl(x))
 
 
-    def count_hsk(self, column):
+    def count_hsk(self, hanyu_col):
         '''counts number of words in vocab that are part of the given hsk list.
         evaluates on word level (as opposed to self.column_lvl(), which evaluates
         on char level)
@@ -45,22 +45,38 @@ class hskInterpreter():
         level: int, optional, return number of words in given level, return all
             level counts if level is none
         '''
-        counted = (column.to_frame()
+        counted = (hanyu_col.to_frame()
                    .merge(self.vocab[['Hanzi', 'Level']], on='Hanzi', how='left')
                    .fillna('Not in HSK'))
 
         return counted['Level'].value_counts()
 
     
-    def plot_hsk_levels(self, hanzi_series):
+    def plot_hsk_levels(self, hanyu_col):
         '''plots hsk levels of characters in a donut chart'''
         
-        plt.pie(hanzi_series, labels=hanzi_series.index)
+        plt.pie(hanyu_col, labels=hanyu_col.index)
         inner_circle = plt.Circle( (0,0), 0.7, color='white')
         p = plt.gcf()
         p.gca().add_artist(inner_circle)
         
         plt.show()
+    
+    
+    def filter_by_level(self, hanyu_col, level):
+        '''filters column by the characters in chars that are in given level or lower
+    
+        hanyu_col: pd.Series, column with words or sentences
+        level: integer, hsk level by which data should be filtered
+        '''
+        
+        extract_hanzi = lambda s: re.sub('[A-Za-z]+', '', ''.join([c for c in s if c.isalnum()]))
+        hanyu_col = hanyu_col.apply(extract_hanzi)
+
+        lvl_col = hanyu_col.apply(self.sentence_lvl) <= level
+        hanyu_col = hanyu_col[lvl_col]
+
+        return hanyu_col
 
 hsk = hskInterpreter()
 unit_tests.test_all(hsk)
--- unit_tests.py
+++ unit_tests.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 def test_all(hsk):
 
+
     assert (hsk.char_lvl('话') == 1 and
         hsk.char_lvl('给') == 2 and
         hsk.char_lvl('干') == 3 and
@@ -11,11 +12,10 @@ def test_all(hsk):
         hsk.char_lvl('抓') == 5 and
         hsk.char_lvl('颇') == 6), 'char_lvl test failed'
     
-    
     assert (hsk.sentence_lvl('你今天怎么样?') == 1 and
             hsk.sentence_lvl('阿姨， 帮我把！') == 3 and
             hsk.sentence_lvl('你抽烟吗?') == 4), 'sentence_lvl test failed'
-    
+
     assert (hsk.column_lvl(pd.Series(['婆婆', '加班', '看法'])) == pd.Series([5,3,3])).all(),\
         'column_lvl test failed'
     
@@ -24,3 +24,8 @@ def test_all(hsk):
         == pd.Series([3,2,2], index = ([3.0, 'Not in HSK', 4.0]))).all(),\
         'count_hsk test failed'
 
+    assert (hsk.filter_by_level(
+        pd.Series(['好棒啊', '加班', '看法', '我们一起去厨房吧', '啤酒很难喝', '老师好'], name='Hanzi'),
+        3)
+        == pd.Series(['加班', '看法', '啤酒很难喝', '老师好'], name='Hanzi')),\
+        'filter_by_level test failed'
